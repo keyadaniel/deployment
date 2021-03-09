7f4af481a63e955589831385e9e5f0295173ee9a
keyadaniel <74474124+keyadaniel@users.noreply.github.com>
2021-03-09 11:28:14 -0600

    first commit

---
+++ src/main/java/com/tts/deployment/controller/TelevisionController.java
@@ -0,0 +1,17 @@
+package com.tts.deployment.controller;
+
+import com.tts.deployment.model.Television;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/television")
+@RestController
+public class TelevisionController {
+
+    @GetMapping("/get")
+    public Television getTelevision(){
+        return new Television(1L, "1080p", "30", "XR35", "Sony");
+    }
+
+}
